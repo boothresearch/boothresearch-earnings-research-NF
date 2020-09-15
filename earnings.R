1e9ebad6363540e04acc5a92409e820ee6eca512
NobuyukiFuruta <nfuruta@uchicago.edu>
2020-09-15 20:37:55 +0000

    task03

--- earnings.R
+++ earnings.R
@@ -1,12 +1,27 @@
 earnings <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Earnings/data/earnings.csv")
 
 sink("reg01.txt", append = FALSE)
-fitted.model <- lm(earnings$earn ~ earnings$height)
+fitted.model <- lm(earnings$earn ~ earnings$height + earnings$male)
 print(Sys.time(), quote = FALSE)
 print(summary(fitted.model))
 closeAllConnections()
+fitted.model
+
 
 png("reg01.png")
-plot(earnings$height, earnings$earn)
+plot(earnings$height, earnings$earn, xlab = "height", ylab="earnings")
+abline(fitted.model)
+dev.off()
+
+sink("reg02.txt", append = FALSE)
+fitted.model2 <- lm(earnings$earn ~ earnings$height + earnings$male + earnings$height * earnings$male)
+print(Sys.time(), quote = FALSE)
+print(summary(fitted.model2))
+closeAllConnections()
+fitted.model
+
+
+png("reg02.png")
+plot(earnings$height, earnings$earn, xlab = "height", ylab="earnings")
 abline(fitted.model)
 dev.off()
